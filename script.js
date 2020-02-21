3a7a53fef53f9c5ce4d5e3251792c937e36805df
Atekawere <jacintawere2@gmail.com>
2020-02-21 09:41:01 +0300

    tried to increase the string value

---
+++ script.js
@@ -0,0 +1,10 @@
+function stringIncrement(strng){
+    var  typeString =[type.split(strng)];
+    var result =[];
+    for(var i=strng.length; i=0;i--){
+        if(i%2==0){
+            result.push(i)
+        }
+
+    }
+}
